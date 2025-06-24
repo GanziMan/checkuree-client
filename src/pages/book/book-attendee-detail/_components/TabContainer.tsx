f27da29e8532a849c1b079889ae50eb14c022ee5
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-06-24 18:53:21 +0900

    refactor: Simplify TabContainer component structure and improve readability

--- src/pages/book/book-attendee-detail/_components/TabContainer.tsx
+++ src/pages/book/book-attendee-detail/_components/TabContainer.tsx
@@ -1,110 +1,43 @@
 import { Content, List, Root, Trigger } from '@radix-ui/react-tabs'
-import StudentManage, { StudentManageProps } from './StudentManage'
-import AttendanceManage, { AttendanceManageProps } from './AttendanceManage'
-import LearningManage, { LearningManageProps } from './LearningManage'
-import CounselManage, { CounselManageProps } from './CounselManage'
+import StudentManage from './StudentManage'
+import AttendanceManage from './AttendanceManage'
+import LearningManage from './LearningManage'
+import CounselManage from './CounselManage'
 import { useParams, useSearchParams } from 'react-router-dom'
 import { useAttendeeDetail } from '../queries'
 import { useEffect } from 'react'
 import Loading from '@/components/Loading'
-import type { CSSProperties, ComponentType } from 'react'
+
+const Menu = [
+  {
+    value: 'tab1',
+    label: '학생관리',
+  },
+  {
+    value: 'tab2',
+    label: '출석관리',
+  },
+  {
+    value: 'tab3',
+    label: '학습관리',
+  },
+  {
+    value: 'tab4',
+    label: '상담관리',
+  },
+]
 
 export default function TabContainer() {
   const { bookId, attendeeId } = useParams()
 
   const [searchParams] = useSearchParams()
+  const scheduleDays = searchParams.get('scheduleDays')
+  const grade = searchParams.get('grade')
 
   const { data: attendeeDetail, refetch } = useAttendeeDetail({
     attendeeId: Number(attendeeId),
     bookId: Number(bookId),
   })
-
-  const defaultStyles: Record<'trigger' | 'content', CSSProperties> = {
-    trigger: { wordBreak: 'keep-all' },
-    content: { background: '#f6f6f6' },
-  }
-
-  const scheduleDays = searchParams.get('scheduleDays')!
-  const grade = searchParams.get('grade')!
-
-  if (!attendeeDetail) return <Loading />
-
-  const studentInfoBase = {
-    name: attendeeDetail.name,
-    age: Number(attendeeDetail.age),
-    grade,
-    scheduleDays,
-  }
-
-  const studentAssociate = attendeeDetail.associates?.find(
-    (fam) => fam.relationType === 'MOTHER' || fam.relationType === 'FATHER',
-  )
-
-  const tabItems: Array<{
-    value: string
-    label: string
-    Component: ComponentType<any>
-    props:
-      | StudentManageProps
-      | AttendanceManageProps
-      | LearningManageProps
-      | CounselManageProps
-  }> = [
-    {
-      value: 'tab1',
-      label: '학생관리',
-      Component: StudentManage,
-      props: {
-        student: {
-          name: attendeeDetail.name,
-          age: Number(attendeeDetail.age),
-          phoneNumber: attendeeDetail.phoneNumber,
-          enrollDate: attendeeDetail.enrollmentDate,
-        },
-        futureSchedules: attendeeDetail.futureSchedules,
-        lessonInfo: attendeeDetail.progresses,
-        registerInfo: {
-          address_1: attendeeDetail.address_1,
-          birthDate: attendeeDetail.birthDate,
-          gender: attendeeDetail.gender,
-          phoneNumber: attendeeDetail.phoneNumber,
-          description: attendeeDetail.description,
-          school: attendeeDetail.school || '',
-        },
-        scheduleItems: attendeeDetail.schedules.schedules || [],
-        associates: {
-          relation: studentAssociate?.relationType || '',
-          phoneNumber: studentAssociate?.phoneNumber || '',
-        },
-      },
-    },
-    {
-      value: 'tab2',
-      label: '출석관리',
-      Component: AttendanceManage,
-      props: { studentInfo: studentInfoBase },
-    },
-    {
-      value: 'tab3',
-      label: '학습관리',
-      Component: LearningManage,
-      props: {
-        progresses: attendeeDetail.progresses,
-        studentInfo: studentInfoBase,
-      },
-    },
-    {
-      value: 'tab4',
-      label: '상담관리',
-      Component: CounselManage,
-      props: {
-        studentInfo: {
-          ...studentInfoBase,
-          associates: attendeeDetail.associates || [],
-        },
-      },
-    },
-  ]
 
   // 뒤로가기 후 데이터를 강제로 다시 불러오기
   // 커리큘럼/클래스(스케쥴) 수정 후 뒤로가기 시 데이터 갱신
@@ -112,6 +45,11 @@ export default function TabContainer() {
     refetch() // React Query의 refetch 기능 실행
   }, [])
 
+  const studentAssociate = attendeeDetail?.associates?.filter(
+    (fam) => fam.relationType === 'MOTHER' || fam.relationType === 'FATHER',
+  )
+
+  if (!attendeeDetail) return <Loading />
   return (
     <Root
       className="TabsRoot"
@@ -121,28 +59,106 @@ export default function TabContainer() {
       }}
     >
       <List className="TabsList" aria-label="Manage your account">
-        {tabItems.map((item) => (
+        {Menu.map((menu) => (
           <Trigger
-            key={item.value}
+            key={menu.label}
             className="TabsTrigger"
-            value={item.value}
-            style={defaultStyles.trigger}
+            value={menu.value}
+            style={{
+              wordBreak: 'keep-all',
+            }}
           >
-            {item.label}
+            {menu.label}
           </Trigger>
         ))}
       </List>
-
-      {tabItems.map((item) => (
-        <Content
-          key={item.value}
-          className="TabsContent"
-          value={item.value}
-          style={defaultStyles.content}
-        >
-          <item.Component {...item.props} />
-        </Content>
-      ))}
+      <Content
+        className="TabsContent"
+        value="tab1"
+        style={{
+          background: '#f6f6f6',
+        }}
+      >
+        <StudentManage
+          student={{
+            name: attendeeDetail.name,
+            age: Number(attendeeDetail.age),
+            phoneNumber: attendeeDetail.phoneNumber,
+            enrollDate: attendeeDetail.enrollmentDate,
+          }}
+          futureSchedules={attendeeDetail.futureSchedules}
+          lessonInfo={attendeeDetail.progresses}
+          registerInfo={{
+            address_1: attendeeDetail.address_1,
+            birthDate: attendeeDetail.birthDate,
+            gender: attendeeDetail.gender,
+            phoneNumber: attendeeDetail.phoneNumber,
+            description: attendeeDetail.description,
+            school: attendeeDetail.school || '',
+          }}
+          scheduleItems={attendeeDetail.schedules.schedules || []}
+          associates={{
+            relation:
+              (studentAssociate || [])?.length > 0
+                ? studentAssociate![0].relationType
+                : '',
+            phoneNumber:
+              (studentAssociate || [])?.length > 0
+                ? studentAssociate![0].phoneNumber
+                : '',
+          }}
+        />
+      </Content>
+      <Content
+        className="TabsContent"
+        value="tab2"
+        style={{
+          background: '#f6f6f6',
+        }}
+      >
+        <AttendanceManage
+          studentInfo={{
+            name: attendeeDetail.name,
+            age: Number(attendeeDetail.age),
+            grade: grade!,
+            scheduleDays: scheduleDays!,
+          }}
+        />
+      </Content>
+      <Content
+        className="TabsContent"
+        value="tab3"
+        style={{
+          background: '#f6f6f6',
+        }}
+      >
+        <LearningManage
+          progresses={attendeeDetail.progresses}
+          studentInfo={{
+            name: attendeeDetail.name,
+            age: Number(attendeeDetail.age),
+            grade: grade!,
+            scheduleDays: scheduleDays!,
+          }}
+        />
+      </Content>
+      <Content
+        className="TabsContent"
+        value="tab4"
+        style={{
+          background: '#f6f6f6',
+        }}
+      >
+        <CounselManage
+          studentInfo={{
+            name: attendeeDetail.name,
+            age: Number(attendeeDetail.age),
+            grade: grade!,
+            scheduleDays: scheduleDays!,
+            associates: attendeeDetail.associates || [],
+          }}
+        />
+      </Content>
     </Root>
   )
 }
